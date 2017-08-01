d92847614c06b55dd22618a35de1746bbf41ca7d
lambdadroid <lambdadroid@gmail.com>
2017-08-01 19:18:48 +0200

    Initial Commit

---
+++ proprietary.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := vendor/asus/me176c/proprietary
+
+# Original ASUS system
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/upi_ug31xx:root/sbin/upi_ug31xx:asus
+
+# Houdini
+LOCAL_PATH := $(LOCAL_PATH)/houdini
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/houdini:system/bin/houdini:intel \
+    $(LOCAL_PATH)/libhoudini.so:system/lib/libhoudini.so:intel \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/arm,system/lib/arm)
---
+++ setup.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+SOURCE_DIR="$PWD"
+TARGET_DIR="$SOURCE_DIR/proprietary"
+
+# Fail if an error occurs
+set -e
+
+if [ -z "$ANDROID_BUILD_TOP" ]; then
+    echo "Please run 'source build/envsetup.sh' before running this script."
+    exit 1
+fi
+
+function download {
+    file="$1"
+    url="$2"
+    md5="$3 $file"
+
+    # Check if md5sum matches local file
+    md5sum -c <<< "$md5" 2> /dev/null && return
+
+    # Download file
+    echo "Downloading $file..."
+    curl -Lo $file "$url"
+
+    # Check again if new file matches md5sum
+    md5sum -c <<< "$md5" && return
+
+    # Remove invalid file
+    rm -f "$file"
+
+    # Report error
+    echo "Cannot download $file. Please try again later."
+    return 1
+}
+
+echo "Downloading files..."
+
+# Original ASUS system
+ASUS_VERSION="UL-K013-WW-12.10.1.36-user"
+download "$ASUS_VERSION.zip" "http://dlcdnet.asus.com/pub/ASUS/EeePAD/ME176C/$ASUS_VERSION.zip" 60ba4a2068e4e8140a6c2accb7c83d19
+download houdini.sfs "http://goo.gl/JsoX2C" b126529f9d78b5b5b7f8c9ff650f6e71
+
+echo "Deleting old files"
+rm -rf "$TARGET_DIR"
+mkdir "$TARGET_DIR"
+
+TEMP_DIR=`mktemp -d`
+cd "$TEMP_DIR"
+
+echo "Extracting boot image"
+unzip -q "$SOURCE_DIR/$ASUS_VERSION.zip" boot.img
+
+echo "Extracting ramdisk"
+python "$ANDROID_BUILD_TOP/system/core/mkbootimg/unpackbootimg" -i boot.img &> /dev/null || :
+
+echo "Unpacking ramdisk"
+mkdir ramdisk && cd ramdisk
+cat ../boot.img-ramdisk.gz | gunzip | cpio -Vid --quiet
+
+echo "Copying files"
+cp sbin/upi_ug31xx "$TARGET_DIR"
+
+cd "$SOURCE_DIR"
+rm -rf "$TEMP_DIR"
+
+echo "Patching files"
+
+# /config partition is used by configfs so we move it to /oemcfg
+sed -i 's@/config/@/oemcfg/@g' "$TARGET_DIR/upi_ug31xx"
+
+echo "Extracting native bridge"
+mkdir "$TARGET_DIR/houdini"
+unsquashfs -d "$TARGET_DIR/houdini/arm" houdini.sfs
+mv "$TARGET_DIR/houdini/arm/"{houdini,libhoudini.so} "$TARGET_DIR/houdini"
+
