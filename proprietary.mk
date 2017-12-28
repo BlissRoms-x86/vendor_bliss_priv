9936634137e9f8dddc4dd0e02e943fd8f05a781f
lambdadroid <lambdadroid@gmail.com>
2017-12-28 19:11:14 +0100

    Copy media_profiles.xml from stock ROM

--- proprietary.mk
+++ proprietary.mk
@@ -6,6 +6,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += $(LOCAL_PATH)/asus
 # Original ASUS system
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/upi_ug31xx:root/sbin/upi_ug31xx:asus \
+    $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml:asus \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/firmware,system/vendor/firmware)
 
 # Widevine DRM
