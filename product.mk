ac5eba7388e83e7c344adf7241325104c61f66d8
lambdadroid <lambdadroid@gmail.com>
2018-10-27 15:24:53 +0200

    Remove proprietary MediaSDK (hardware accelarated codecs) libraries
    Broken on Android 9 (Pie).

--- product.mk
+++ product.mk
@@ -9,12 +9,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml:asus \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/firmware,system/vendor/firmware)
 
-# MediaSDK OMX plugin (hardware accelerated codecs)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/libmfx_omx_core.so:system/vendor/lib/libmfx_omx_core.so:intel \
-    $(LOCAL_PATH)/media/libmfx_omx_components_hw.so:system/vendor/lib/libmfx_omx_components_hw.so:intel \
-    $(LOCAL_PATH)/media/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf:intel
-
 # Widevine DRM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so:widevine
