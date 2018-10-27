f97609099490b3220b5b2d7cadcf870fcae02c23
lambdadroid <lambdadroid@gmail.com>
2018-10-27 17:07:17 +0200

    Use SHA-256 instead of SHA-1 for checking source files

--- setup.sh
+++ setup.sh
@@ -5,16 +5,16 @@ export TARGET_DIR="$SOURCE_DIR/proprietary"
 export ASUS_VERSION="UL-K013-WW-12.10.1.36-user"
 ASUS_DOWNLOAD="$ASUS_VERSION.zip"
 ASUS_DOWNLOAD_URL="http://dlcdnet.asus.com/pub/ASUS/EeePAD/ME176C/$ASUS_DOWNLOAD"
-ASUS_DOWNLOAD_SHA1="73255a5d4c87998ab90fbd2606317753ad1cc4f7"
+ASUS_DOWNLOAD_SHA256="b19a2901bd5920b58bd3693243a9edf433656bcee8f454637ee401e28c096469"
 
 export CHROMEOS_VERSION="chromeos_10176.76.0_eve_recovery_stable-channel_mp"
 CHROMEOS_DOWNLOAD="$CHROMEOS_VERSION.bin.zip"
 CHROMEOS_DOWNLOAD_URL="https://dl.google.com/dl/edgedl/chromeos/recovery/$CHROMEOS_DOWNLOAD"
-CHROMEOS_DOWNLOAD_SHA1="68b6f0eef796363cc67f5836de581f70ea62e19a"
+CHROMEOS_DOWNLOAD_SHA256="00b3c9406effd0f3fe83a65c7f5440eb677bd9bdec35e0c2fe048045e3aebc2f"
 
 LENOVO_DOWNLOAD="gwuj26ww.exe"
 LENOVO_DOWNLOAD_URL="https://download.lenovo.com/pccbbs/mobiles/$LENOVO_DOWNLOAD"
-LENOVO_DOWNLOAD_SHA1="6585c2e55904d2614ad6c1bfa0e7021335a0939d"
+LENOVO_DOWNLOAD_SHA256="baf9e5d86805de278f94f9de91809de0cdbbce2c5f91350ea9641fc10dccd895"
 
 # Fail if an error occurs
 set -e
@@ -25,7 +25,7 @@ function download {
     sha1="$3 $file"
 
     # Check if sha1sum matches local file
-    sha1sum -c <<< "$sha1" 2> /dev/null && return
+    sha256sum -c <<< "$sha1" 2> /dev/null && return
 
     # Download file
     echo "Downloading $file..."
@@ -44,9 +44,9 @@ function download {
 
 echo "Downloading files..."
 cd "$SOURCE_DIR"
-download "$ASUS_DOWNLOAD" "$ASUS_DOWNLOAD_URL" "$ASUS_DOWNLOAD_SHA1"
-download "$CHROMEOS_DOWNLOAD" "$CHROMEOS_DOWNLOAD_URL" "$CHROMEOS_DOWNLOAD_SHA1"
-download "$LENOVO_DOWNLOAD" "$LENOVO_DOWNLOAD_URL" "$LENOVO_DOWNLOAD_SHA1"
+download "$ASUS_DOWNLOAD" "$ASUS_DOWNLOAD_URL" "$ASUS_DOWNLOAD_SHA256"
+download "$CHROMEOS_DOWNLOAD" "$CHROMEOS_DOWNLOAD_URL" "$CHROMEOS_DOWNLOAD_SHA256"
+download "$LENOVO_DOWNLOAD" "$LENOVO_DOWNLOAD_URL" "$LENOVO_DOWNLOAD_SHA256"
 
 echo "Deleting old files"
 rm -rf "$TARGET_DIR"
