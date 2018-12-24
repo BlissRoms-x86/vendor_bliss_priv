bac70489f90bc680f692c393eb2f12bcf301ffde
lambdadroid <lambdadroid@gmail.com>
2018-12-24 10:56:56 +0100

    Fix update_engine_applier not being built before setup.sh

--- Android.mk
+++ Android.mk
@@ -19,13 +19,14 @@ $(LOCAL_BUILT_MODULE): $(TARGET_KERNEL_DEFCONFIG) | dsdt.hex
 TARGET_KERNEL_DEFCONFIG := $(LOCAL_BUILT_MODULE)
 
 # Download and extract proprietary files
+UPDATE_ENGINE_APPLIER := $(HOST_OUT_EXECUTABLES)/update_engine_applier
+
 .PHONY: proprietary
 proprietary: $(UPDATE_ENGINE_APPLIER)
 	UPDATE_ENGINE_APPLIER=$(abspath $(UPDATE_ENGINE_APPLIER)) \
 		$(LOCAL_PATH)/setup.sh $(abspath $(LOCAL_PATH))
 
 proprietary: LOCAL_PATH := $(LOCAL_PATH)
-proprietary: UPDATE_ENGINE_APPLIER := $(HOST_OUT_EXECUTABLES)/update_engine_applier
 
 # Avoid output buffering since it infers with the sudo confirmation prompt
 proprietary: .KATI_NINJA_POOL := console
